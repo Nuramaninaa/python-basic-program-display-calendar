8001098cc898f391ab20842d8bd8691021e4477e
Nuramaninaa <60954922+Nuramaninaa@users.noreply.github.com>
2023-07-18 19:40:03 +0800

    Update display_calendar.py

--- display_calendar.py
+++ display_calendar.py
@@ -1,10 +1,11 @@
 #python program to display calendar
 
-
+#import calendar module
 import calendar
 
+#take input from user for year and month
 year = int(input("Enter the number of year : "))
 month = int(input("Enter the number of month : "))
 
-
+#output of calendar
 print(calendar.month(year,month)) 
